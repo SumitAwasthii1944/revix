baf4c695c9d3d400eeb250f2fd1f00650f04aab8
SumitAwasthii1944 <sumit.24660@knit.ac.in>
2026-07-29 03:27:15 +0530

    final fix

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = process.env.NEXT_PUBLIC_APP_URL
+const baseURL = process.env.APP_URL
   ? `${process.env.APP_URL}/api`
   : "http://localhost:3000/api"
 
