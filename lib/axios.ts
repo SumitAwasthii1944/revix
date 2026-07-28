78bd50e468d581f13825afb45dabe7843cdd01e4
SumitAwasthii1944 <sumit.24660@knit.ac.in>
2026-07-29 03:36:13 +0530

    adding NEXT_PUBLIC is necessary in axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-const baseURL = process.env.APP_URL
-  ? `${process.env.APP_URL}/api`
+const baseURL = process.env.NEXT_PUBLIC_APP_URL
+  ? `${process.env.NEXT_PUBLIC_APP_URL}/api`
   : "http://localhost:3000/api"
-
+  
 export const axiosInstance = axios.create({
   baseURL,
   headers: {
